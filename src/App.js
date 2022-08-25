f0b529b74e0bf1790625711f68960d55aab8d4b1
haotian <haotian.liu@thoughtworks.com>
2022-08-25 21:21:50 +0800

    hal fix lint errors

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from "react";
 import Entry from "./component/Entry";
 
 const entries = [
-  { date: "December-01-2011", desc: "House Insurance", price: "700" },
-  { date: "January-01-2012", desc: "New car", price: "3000" },
-  { date: "November-06-2013", desc: "New TV", price: "350" },
+  { id: 1, date: "December-01-2011", desc: "House Insurance", price: "700" },
+  { id: 2, date: "January-01-2012", desc: "New car", price: "3000" },
+  { id: 3, date: "November-06-2013", desc: "New TV", price: "350" },
 ];
 
 const App = () => {
   return (
     <div className="expense">
       {entries.map((entry) => (
-        <Entry props={entry} />
+        <Entry key={entry.id} props={entry} />
       ))}
     </div>
   );
